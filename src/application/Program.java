13638a26eb3b5ccdd51d3fef398096b1f90305dd
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-08-27 17:31:54 +0100

    SellerDaoJDBC && DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,6 +18,9 @@ public class Program {
 		System.out.println(obj);
 		
 		Seller sellerObj = new Seller(21, "Bob", "bob@gmail.com", LocalDate.parse("1986-04-08", formatter), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(sellerObj);
 	}
 
