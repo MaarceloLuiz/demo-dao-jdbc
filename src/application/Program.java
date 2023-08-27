3cb4ddb0c2a09abfdfdad1ac9205a4326cef9b4a
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-08-27 15:15:30 +0100

    Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,22 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		
+		Department obj = new Department(1, "Books");
 		System.out.println(obj);
-
+		
+		Seller sellerObj = new Seller(21, "Bob", "bob@gmail.com", LocalDate.parse("1986-04-08", formatter), 3000.0, obj);
+		System.out.println(sellerObj);
 	}
 
 }
