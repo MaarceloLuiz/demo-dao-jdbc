94c7c2fe425a39b10e9d36f7f1ddff8186eec072
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-08-28 13:14:54 +0100

    findById Implementation && Database Script

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,21 @@
 package application;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller sellerObj = new Seller(21, "Bob", "bob@gmail.com", LocalDate.parse("1986-04-08", formatter), 3000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(sellerObj);
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
+		
+		//the info is going to be collected from our database/
+		//is going to be printed the seller's info found in the id 3 row
 	}
 
 }
